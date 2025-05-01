cf52515e783fb976eabb29e094771c7a40a31ae2
park2061 <pbk2061@naver.com>
2025-05-01 15:22:51 +0900

    기존 회원 탈퇴 기능 수정

--- petplace/src/main/java/com/petplace/repository/PostRepository.java
+++ petplace/src/main/java/com/petplace/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.petplace.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,5 +15,7 @@ public interface PostRepository extends JpaRepository<Post,Long> {
     Page<Post> findByTitleContaining(String keyword,Pageable pageable);
     List<Post> findByTitleContaining(String keyword);
 
-
+    // 사용자 ID로 게시물 삭제 메소드 추가
+    @Transactional
+    void deleteByUser_UserId(Long userId);
 }
